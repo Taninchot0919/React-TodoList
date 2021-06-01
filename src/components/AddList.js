1a586f8004e1ad0b43d09ddce09e8bee0feb7e58
Taninchot Art <Taninchot.art@mail.kmutt.ac.th>
2021-06-01 10:17:46 +0700

    [UPDATE] Use can use method post

--- src/components/AddList.js
+++ src/components/AddList.js
@@ -1,15 +1,50 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+
 const AddList = () => {
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const history = useHistory();
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    let newList = {
+      title: title,
+      status: "On going",
+      details: details,
+    };
+    console.log(newList);
+    fetch("http://localhost:9000/Lists", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newList),
+    }).then((res) => {
+      if (res.ok) {
+        history.push("/");
+      }
+    });
   };
 
   return (
     <div id="add-list" className="flex justify-center w-full">
       <form>
         <label className="text-left block text-xl mt-3">Title : </label>
-        <input type="text" className="block border border-black w-full px-2" />
-        <label className="text-left block text-xl mt-3">Details : </label>
-        <textarea className="block border border-black w-full h-32 px-2"></textarea>
+        <input
+          type="text"
+          className="block border border-black max-w-full px-2"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+        />
+        {/* {title} */}
+        <label className="text-left block text-xl mt-3">Details :</label>
+        <textarea
+          className="block border border-black max-w-full h-32 px-2"
+          onChange={(e) => {
+            setDetails(e.target.value);
+          }}
+        />
+        {/* {details} */}
         <button
           className="px-3 py-1 mt-3 border-black border"
           onClick={handleSubmit}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,7 +2,7 @@ const TodoList = ({ lists }) => {
   console.log(lists);
   return (
     <div className="space-y-3 my-3">
-      {lists.map((list,index) => (
+      {lists.map((list, index) => (
         // จำไว้ว่า map แล้วต้องเป็น () ไม่ใช่ {}
         <div key={index} className="border border-gray-300 rounded-md p-3">
           <p className="text-lg font-semibold">Title : {list.title}</p>
